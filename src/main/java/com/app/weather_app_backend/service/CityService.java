9e3d74f41108196e7eff367193ab955ac91791e5
juanlunaa <jp3165239@gmail.com>
2024-07-18 17:07:30 -0500

    Create service to get location of the user

--- src/main/java/com/app/weather_app_backend/service/CityService.java
+++ src/main/java/com/app/weather_app_backend/service/CityService.java
@@ -1,16 +1,24 @@
 package com.app.weather_app_backend.service;
 
+import com.app.weather_app_backend.exceptions.ResourceNotFoundException;
 import com.app.weather_app_backend.model.City;
 import com.app.weather_app_backend.repository.CityRepository;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class CityService {
     @Autowired
     private CityRepository cityRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public List<City> getCitiesByName (String name) {
         return cityRepository.findCityByName(name);
@@ -18,5 +26,15 @@ public class CityService {
 
     public List<City> getCitiesByNameAndCountry (String nameCity, String codeCountry) {
         return cityRepository.findCityByNameAndCountry(nameCity, codeCountry);
+    }
+
+    public Map<String, Object> getLocalCityInfo (String ip, String API_KEY) {
+        try {
+            String API_URL = String.format("https://ipinfo.io/%s?token=%s", ip, API_KEY);
+            String res = restTemplate.getForObject(API_URL, String.class);
+            return new JSONObject(res).toMap();
+        } catch (HttpClientErrorException.NotFound e) {
+            throw new ResourceNotFoundException("Location not found");
+        }
     }
 }
